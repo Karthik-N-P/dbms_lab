a641ebb0c39c4ff606600dee0701cd75ca4fb2ab
KARTHIK N P <79256465+Karthik-N-P@users.noreply.github.com>
2022-11-13 12:26:23 +0530

    Update 21-25.sql

--- 21-25.sql
+++ 21-25.sql
@@ -1,2 +1,12 @@
+--21) delete the dpt no 3 from database
+
+
+
+-- 24) change the location dnum of projectno 10 to kerala and 5 
+UPDATE PROJECT set plocation='kolkata', dnum=5 where PNUMBER=2;
+SELECT * from PROJECT
+
 --25) give all employee in the sales dpt a 10% hike
 UPDATE EMPLOYEE SET salary= salary + ((salary * 10) / 100) WHERE dno in(SELECT dnumber  from DEPARTMENT where dname='Sales');
+
+
